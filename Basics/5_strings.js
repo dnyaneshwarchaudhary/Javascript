e3245d39aac44321b3dea2577651557afb3461bd
Dnyaneshwar Chaudhary <80617031+dnyaneshwarchaudhary@users.noreply.github.com>
2023-10-09 10:12:51 +0000

    updates course

---
+++ Basics/5_strings.js
@@ -0,0 +1,14 @@
+const name = "Dnyaneshwar Chaudhary";
+const age = 21;
+
+console.log(`Hello, my name is ${name} and my age is ${age}`);
+
+const gameName = new String("  Dnyaneshwar Chaudhary  ");
+
+console.log(gameName.__proto__);
+console.log(gameName.toLowerCase());
+console.log(gameName);
+console.log(gameName.charAt(2));
+
+console.log(gameName.trim());
+
---
+++ Basics/6_numbersandmaths.js
@@ -0,0 +1,26 @@
+const score = 300;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+
+const otherNumber = 123.524;
+console.log(otherNumber.toPrecision(2));
+console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000000
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ------------------- Maths -------------------
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.floor(4.9));
