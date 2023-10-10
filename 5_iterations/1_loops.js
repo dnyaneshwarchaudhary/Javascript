0629bcaffdf678cbf9e06c5e5e8f30f60403e601
Dnyaneshwar Chaudhary <80617031+dnyaneshwarchaudhary@users.noreply.github.com>
2023-10-10 06:37:38 +0000

    loops basic and advan

---
+++ 5_iterations/1_loops.js
@@ -0,0 +1,16 @@
+// for 
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+}
+
+
+
+do {
+    console.log(
+    "i am in a do while loop"
+    );
+    
+} while (true);
+
